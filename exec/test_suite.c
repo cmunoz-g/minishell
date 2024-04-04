7b1ae9c3eeac06a01cb2e79f18787aa9858140e7
juramos <juramos@student.42madrid.com>
2024-04-04 13:40:44 +0200

    added test suite

---
+++ exec/test_suite.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_suite.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 12:40:49 by juramos           #+#    #+#             */
+/*   Updated: 2024/04/04 13:39:46 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_suite.h"
+
+/*
+	Por ahora solo gestionamos un arg
+*/
+
+void	print_cmd(t_cmd_table *tbl)
+{
+	int	i;
+
+	i = 0;
+	if (!tbl)
+		;
+	printf("cmd: %s\n", tbl->cmd);
+	printf("arg %s\n", tbl->arg);
+	printf("in: %d\n", tbl->in);
+	printf("out: %d\n", tbl->out);
+	printf("--------\n");
+}
+
+t_cmd_table	*init_cmd(char *cmd, char *arg, int in, int out)
+{
+	t_cmd_table	*t;
+
+	t = malloc(sizeof(t_cmd_table));
+	if (!t)
+		exit(0);
+	t->cmd = malloc(sizeof(char) * strlen(cmd));
+	strcpy(t->cmd, cmd);
+	t->arg = malloc(sizeof(char) * strlen(arg));
+	strcpy(t->arg, arg);
+	t->in = in;
+	t->out = out;
+	t->next = NULL;
+	t->prev = NULL;
+	return (t);
+}
+
+int	main(void)
+{
+	t_cmd_table	*tbl;
+	t_cmd_table	*tbl_2;
+
+	tbl = init_cmd("ls", "-la", STDIN, PIPE);
+	tbl_2 = init_cmd("grep", "a", PIPE, FILE);
+	tbl->next = tbl_2;
+	tbl_2->prev = tbl;
+	while (tbl)
+	{
+		print_cmd(tbl);
+		tbl = tbl->next;
+	}
+	return (0);
+}
---
+++ exec/test_suite.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_suite.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 12:59:41 by juramos           #+#    #+#             */
+/*   Updated: 2024/04/04 13:33:55 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_SUITE_H
+# define TEST_SUITE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+# define EMPTY 0
+# define CMD 1
+# define ARG 2
+# define TRUNC 3
+# define APPEND 4
+# define INPUT 5
+# define PIPE 6
+# define END 7
+# define STDIN 8
+# define FILE 9
+
+typedef struct s_token
+{
+	int				type;
+	char			*value;
+	struct s_token	*next;
+	struct s_token	*prev;
+}				t_token;
+
+typedef struct s_cmd_table
+{
+	char				*cmd;
+	char				*arg;
+	int					in;
+	int					out;
+	struct s_cmd_table	*prev;
+	struct s_cmd_table	*next;
+}				t_cmd_table;
+
+t_cmd_table	*init_cmd(char *cmd, char *arg, int in, int out);
+
+#endif

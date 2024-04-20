0c7daae34e36be61fa4c68f3cfd797f17a8b40bf
jjuanramos <jjuanramos91@gmail.com>
2024-04-20 12:36:41 +0200

    solved misuse of ft_substring

--- src/exec/expand.c
+++ src/exec/expand.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 12:22:51 by juramos           #+#    #+#             */
-/*   Updated: 2024/04/20 12:11:39 by juramos          ###   ########.fr       */
+/*   Updated: 2024/04/20 12:35:13 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ static char	*expand_word(char *str, int start, int end, char **envp)
 
 	if (str[start + 1] == '?')
 		return (ft_itoa(g_global.error_num));
-	keyword = ft_substr(str, start + 1, end);
+	keyword = ft_substr(str, start + 1, end - start);
 	keyword_cleaned = ft_strtrim(keyword, " ");
 	value = ft_strdup(my_getenv(keyword_cleaned, envp));
-	return (free(keyword_cleaned), value); // we have stopped doing free(keyword) because it causes double-free error when doing `echo $USER`
+	return (free(keyword_cleaned), value);
 }
 
 static char	*expand_str(char *str, int start, int *i, char **envp)

11aded9804ea4b25bf2789b8481bb4e71ebf4100
jjuanramos <jjuanramos91@gmail.com>
2024-04-26 11:42:14 +0200

    handled bugs related to exit: seg. fault when it was the first command to be used, as well as exitting minishell when invalid input was entered

--- include/minishell.h
+++ include/minishell.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camunozg <camunozg@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:27:08 by juramos           #+#    #+#             */
-/*   Updated: 2024/04/24 14:57:28 by camunozg         ###   ########.fr       */
+/*   Updated: 2024/04/26 11:29:28 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
 # define EMPTY 0
 # define CMD 1
@@ -132,7 +132,7 @@ t_minishell	*init(char **envp);
 /* mini_cd */
 int			mini_cd(t_minishell	*data);
 /* mini_exit */
-void		mini_exit(t_minishell *data);
+int			mini_exit(t_minishell *data);
 /* mini_pwd */
 int			mini_pwd(t_minishell *data);
 /* mini_echo */
@@ -162,7 +162,7 @@ int			ft_isspace(int c);
 int			check_spaces(char *line);
 char		*ft_strdup_mod(const char *s, size_t size);
 void		error(t_minishell *data, char *error_message);
-void		error_builtins(t_minishell *data, int exit_code);
+int			error_builtins(t_minishell *data, int exit_code);
 
 /*	minishell_loop */
 void		minishell_loop(t_minishell *data);

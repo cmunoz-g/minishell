a0036d29cde9930cbaa333b8f20b91234f884ca1
Carlos <carlosmg.mkt@gmail.com>
2024-05-13 12:38:27 +0200

    fixed syntax error

--- src/utils/error.c
+++ src/utils/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmunoz-g <cmunoz-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: camunozg <camunozg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:26:51 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/07 11:03:46 by cmunoz-g         ###   ########.fr       */
+/*   Updated: 2024/05/13 12:37:56 by camunozg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	error_builtins(t_minishell *data, int exit_code)
 	if (data)
 	{
 		if (data->cmd_table)
-			clean_cmd_table_list(&data->cmd_table);
+			clean_cmd_table_list(data->cmd_table);
 		if (data->token_list)
 			clean_token_list(&data->token_list);
 		free(data);

264f99e0b464d7991e9bd5ebeff46951a9b59d8f
jjuanramos <jjuanramos91@gmail.com>
2024-05-15 13:19:29 +0200

    new error code

--- src/builtins/utils_builtins.c
+++ src/builtins/utils_builtins.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:50:59 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/11 14:30:42 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/15 13:18:59 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	simple_builtin_executor(t_minishell *data)
 		ret = redirect(data->cmd_table, 1);
 		if (ret)
 		{
-			g_global.error_num = ret;
+			g_global.error_num = 1;
 			return ;
 		}
 	}
--- src/exec/redirections.c
+++ src/exec/redirections.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:02:55 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/13 09:55:25 by juramos          ###   ########.fr       */
+/*   Updated: 2024/05/15 13:19:17 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ int	redirect_all(t_minishell *data)
 		ret = redirect(data->cmd_table, 0);
 		if (ret)
 		{
-			g_global.error_num = ret;
+			g_global.error_num = 1;
 			return (ret);
 		}
 	}

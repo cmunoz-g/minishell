097b856b3aff97542d673243728e5964a3dd46d3
Carlos <carlosmg.mkt@gmail.com>
2024-05-27 09:18:31 +0200

    added management for no envp

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: camunozg <camunozg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:22:55 by juramos           #+#    #+#             */
-/*   Updated: 2024/05/24 09:42:03 by camunozg         ###   ########.fr       */
+/*   Updated: 2024/05/27 09:18:01 by camunozg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,28 +38,38 @@ int	get_sh_lvl(char **envp)
 		return (1);
 }
 
-t_minishell	*init(char **envp)
+void	init_aux(t_minishell *data)
 {
-	t_minishell	*data;
-
-	data = (t_minishell *)malloc(sizeof(t_minishell));
 	data->token_list = NULL;
 	data->cmd_table = NULL;
 	data->line = NULL;
 	data->pids = NULL;
 	data->fd_in = dup(STDIN_FILENO);
 	data->fd_out = dup(STDOUT_FILENO);
+}
+
+t_minishell	*init(char **envp)
+{
+	t_minishell	*data;
+
+	data = (t_minishell *)malloc(sizeof(t_minishell));
+	if (!data)
+		(printf("Error: Could not alloc memory for data\n"),
+			exit(EXIT_FAILURE));
+	init_aux(data);
 	data->sh_lvl = get_sh_lvl(envp);
 	data->env_vars = ft_arrdup(envp);
-	data->local_vars = NULL;
-	data->export_vars = ft_arrdup(data->env_vars);
 	if (!data->env_vars)
 		error(data, "Could not alloc memory for env. vars.");
+	data->local_vars = NULL;
+	data->export_vars = ft_arrdup(data->env_vars);
+	if (!data->export_vars)
+		error(data, "Could not alloc memory for exp. vars.");
 	data->pwd = ft_strdup(my_getenv("PWD", data->env_vars));
 	if (!data->pwd)
 		error(data, "Could not alloc memory for pwd");
 	data->old_pwd = ft_strdup(my_getenv("OLDPWD", data->env_vars));
-	if (!data->old_pwd) // en mi VSC en casa revienta porque en env no hay old pwd, comprobar en 42
+	if (!data->old_pwd)
 		error(data, "Could not alloc memory for old pwd");
 	g_global.error_num = 0;
 	init_signal_vars();
